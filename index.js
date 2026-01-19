2abf56623cc473f230a9af6fcaf10296d54d8418
Shafay Ahmed <shafayahmed77@gmail.com>
2026-01-19 17:07:06 +0500

    added a var for result paragraph

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 let num1 = 8;
 let num2 = 2;
+let sumEl=document.getElementById ("sum-el");
 document.getElementById("num1-el").textContent = num1;
 document.getElementById("num2-el").textContent = num2;
 
 function add() {
     let sum = num1 + num2;
-    document.getElementById("sum-el").textContent = "Sum : " + sum;
+    sumEl.textContent = "Sum : " + sum;
 }
 
 function sub() {
     let sub =num1 - num2;
-    document.getElementById("sum-el").textContent = "subtract : " + sub;
+    sumEl.textContent = "subtract : " + sub;
 }
 
 function mul() {
     let mul = num1 * num2;
-    document.getElementById("sum-el").textContent = "Multiply : " + mul;
+    sumEl.textContent = "Multiply : " + mul;
 }
 
 function div() {
     let div = num1 / num2;
-    document.getElementById("sum-el").textContent = "divide : " + div;
+    sumEl.textContent = "divide : " + div;
 }
